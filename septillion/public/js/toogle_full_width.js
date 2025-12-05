5f4e3a6255465e608e424d23294bdd9dd87bbff9
Sakshi-Greycube <sakshi@greycube.in>
2025-12-05 13:45:55 +0530

    fix: trigger toggle full width frappe function

--- septillion/public/js/toogle_full_width.js
+++ septillion/public/js/toogle_full_width.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
-    let fullwidth = true;
-    localStorage.container_fullwidth = fullwidth;
-    $(document.body).toggleClass("full-width", fullwidth);
-    console.log(localStorage.container_fullwidth, "=============In Toggle Full Width JS===============");
+    console.log("=============DOCUMENT READY===============");
+    let fullwidth = localStorage.container_fullwidth
+    console.log(fullwidth, "============localStorage.container_fullwidth==========")
+    if (fullwidth == false || fullwidth == "false") {
+        console.log("===============inside if=============")
+        frappe.ui.toolbar.toggle_full_width()
+    }
 });
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   // Code to be executed once the DOM is ready
+//     console.log("DOM is ready!");
+//     let fullwidth = localStorage.container_fullwidth
+//     console.log(fullwidth, "============localStorage.container_fullwidth==========")
+//     if (fullwidth == false || fullwidth == "false") {
+//         console.log("===============inside if=============")
+//         frappe.ui.toolbar.toggle_full_width()
+//     }
+// });
